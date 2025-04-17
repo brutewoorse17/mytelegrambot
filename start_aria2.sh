c2955a4defcff80668ccaa869253f60da85ab3c5
brutewoorse17 <147273128+brutewoorse17@users.noreply.github.com>
2025-04-18 07:55:32 +0800

    rename aria2.sh

---
+++ start_aria2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+aria2c \
+  --enable-rpc \
+  --rpc-listen-all=true \
+  --rpc-allow-origin-all=true \
+  --rpc-listen-port=6800 \
+  --dir=downloads \
+  --max-connection-per-server=10 \
+  --min-split-size=1M \
+  --split=10 \
+  --follow-torrent=mem \
+  --seed-time=0
